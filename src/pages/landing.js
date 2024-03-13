c9bdf095b82f4ec5dc91bc3234e37488f5414278
willow-rubenstein <contact.notashe@gmail.com>
2024-03-13 15:05:17 -0400

    minor styling changes

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -52,7 +52,7 @@ export default function Landing(props) {
                     <span className="landing-caption">
                         <span className="rainbow rainbow_text_animated">
                             It looks like you have already paid for this event!
-                        </span><br/>
+                        </span><br/><br/>
                         If you have left the server by accident, you can be re-invited by pressing the button below!<br/><br/>
                         <RejoinButton />
                     </span>
@@ -65,7 +65,7 @@ export default function Landing(props) {
                         localStorage.getItem("discordtoken") !== null ?
                         <span className="landing-caption">Please press any of the options below to pay for entry and gain access to the Project Muse server!<br/></span>
                         :
-                        <span className="landing-caption">In order to join, please press the button below!<br/><br/></span>
+                        <span className="landing-caption">In order to join, please press the button below!<br/></span>
                     }
                     <br/>
                     {
@@ -94,13 +94,16 @@ export default function Landing(props) {
                 because screens are not sentient. Yes, of course, even my fundraising has to be overcomplicated. Nothing is ever easy with me.
                 Thus is why, for the first time ever, I am doing an uncapped Project Muse donothon.<br/><br/>
                 Here's how it works:<br/>
-                - The show starts with a total of 6 songs. Until those songs are completed, the show will not end.<br/>
-                - Payment is completed through the portal on this site which you gain access to after signing in via discord.<br/>
-                - Once paid, your Discord account will automatically be added to the Project Muse server.<br/>
+                <div style={{display: "flex", flexDirection: "column", rowGap: "5px", marginTop: "5px"}}>
+                    <span>- The show starts with a total of 6 songs. Until those songs are completed, the show will not end.<br/></span>
+                    <span>- Payment is completed through the portal on this site which you gain access to after signing in via discord.<br/></span>
+                    <span>- Once paid, your Discord account will automatically be added to the Project Muse server.<br/></span>
+                </div>
+                
                 <br/>
                 For this one-time event, instead of ending after 6 songs, we will continue going; for every 10 participants who register for this special episode, another song will be made.<br/>
                 <br/>The show will <b>NOT</b> stop until <b>ALL</b> of those slots have been fulfilled.
-                <br/><br/>
+                <br/><br/><br/>
                 <ButtonStuff />
             </span>
             <br/><br/>
