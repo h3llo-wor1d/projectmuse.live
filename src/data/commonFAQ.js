294a76bec59f2c55a2d0ff1f1b368af938189c1b
Wrench <contact.notashe@gmail.com>
2024-07-04 20:57:32 -0500

    god this is such a pain in the ass haha

--- src/data/commonFAQ.js
+++ src/data/commonFAQ.js
@@ -2,5 +2,25 @@ export const commonFAQ = [
     [
         "What should I put in the song reference fields?",
         "Ideally, I want Spotify/YouTube/SoundCloud/etc. links which give me a good idea of the style/genre of music you want me to make for you. Please try to provide one style to the best of your ability, or I will just choose one!"
+    ],
+    [
+        "How long should my music style/genre field be?",
+        "Please do not give me an essay about what I'm making, try to limit it to a few sentences if possible. The description (genre) field exists to fill in the blanks where the references cannot."
+    ],
+    [
+        "How do we know this isn't rigged?",
+        "I already rigged it in the past as an april fools joke. it took me 3 months and 2 servers. it is too much of a pain in the ass. i am never doing that shit again"
+    ],
+    [
+        "Can I use DMCA music/songs with lyrics?",
+        "Absolutely! Do not worry about DMCA at all, the use case for the show falls under fair use. Feel free to add whatever songs you want to the references!"
+    ],
+    [
+        "Can I register without an existing OC?",
+        "Unfortunately, you must have an existing OC to participate! Creating a new OC is very time-consuming and Neptune does not have the energy for that on-stream!"
+    ],
+    [
+        "Can I edit my registration again?",
+        "Absolutely! All registration data (just the stuff on this page!) is stored on our AWS database, you are able to alter that at any point you'd like"
     ]
 ]
--- src/pages/onboarding/infopage.js
+++ src/pages/onboarding/infopage.js
@@ -14,6 +14,9 @@ import RipNTear from '../../audio/RipNTear.flac';
 import { NavigateNext, RemoveCircle } from '@mui/icons-material';
 import i1 from "../../audio/Narrator/i1.flac";
 import { commonFAQ } from '../../data/commonFAQ';
+import dnd from '../../images/status/dnd.png';
+import idle from '../../images/status/idle.png';
+import online from '../../images/status/online.png';
 
 const Page = styled.div `
     padding: 30px 30px 30px 30px;
@@ -170,7 +173,7 @@ export default function InfoPage(props) {
                         position: "fixed",
                         bottom: 0,
                         left: 0,
-                        zIndex: 99999,
+                        zIndex: 99999999,
                         padding: "10px 20px 10px 30px",
                         borderTopRightRadius: "30px",
                         backgroundColor: "rgba(255,255,255,0.15)"
@@ -198,7 +201,7 @@ export default function InfoPage(props) {
                 <InfoBlock subheading={"Information On The Show"} emoji={"💫"} narrator={i1} 
                 tooltip={"Small introductory segment about the show"}
                 p={
-                    <p>
+                    <p style={{marginTop: "5px"}}>
                         A common misconception is that this is just a normal interview show. Project Muse, however, 
                         doubles as a live BGM raffle as well, in which you get picked at random, interviewed, and then 
                         made music for, all in an hour or less.<br/>
@@ -209,7 +212,7 @@ export default function InfoPage(props) {
                 <InfoBlock subheading={"Required Steps To Participate"} 
                 tooltip={"The steps you must take to participate and receive free art and music!"}
                 emoji={"‼"} p={
-                    <p>
+                    <p style={{marginTop: "5px"}}>
                         In order to participate in the show, all participants must fill out the form on the next page. If the form is not filled out, you CANNOT be picked.<br/><br/>
                         The form allows myself and Neptune to have a better idea of the style of art and music you are looking for and provide a better end result.
                         It is around 10 questions, most of which may have been pre-filled using data on your profile.<br/><br/>
@@ -219,7 +222,7 @@ export default function InfoPage(props) {
                 <InfoBlock subheading={"Commonly Asked Questions Regarding Registration"} 
                 tooltip={"Brief FAQ for registration"}
                 emoji={"🤔"} p={
-                    <div style={{marginTop: "5px"}}>
+                    <div style={{marginTop: "5px", display: "grid", gridTemplateColumns: "auto", rowGap: "30px"}}>
                         {
                             commonFAQ.map(i => <div>
                                 <h3>"{i[0]}"</h3>
@@ -230,8 +233,35 @@ export default function InfoPage(props) {
                             </div>)
                         }
                     </div>
-                    
-                } />
+                } /><br/>
+                <InfoBlock subheading={"Information on the picking process"} 
+                tooltip={"Explaining how user selection works for Project Muse"}
+                emoji={"📋"} p={
+                    <p style={{marginTop: "5px"}}>
+                        When the stream starts, on my command, the bot will randomly select <b>REGISTERED</b> and <b>NON-INVISIBLE</b> members to join a channel called ⁠Picked VC. 
+                        If you have been picked, you will be pinged and have <b>60 seconds</b> to join the VC, or you will be skipped! 
+                        Inside the VC, I will interview you, and then Neptune and I will create work based on the form you've submitted! 
+                        <div style={{display: "grid", gridTemplateColumns: "10px auto", columnGap: "10px", marginTop: "10px"}}>
+                            <div style={{width: "8px", height: "100%", borderRadius: "30px", backgroundColor: "grey"}} />
+                            <p>If I do not finish the track and/or Neptune does not finish the art on-stream within the hour time limit (and it is not beyond my ability at the time), we will finish it off-stream and send it to you</p>
+                        </div><br/>
+                        To better clarify non-invisible statuses, here is a list of statuses the bot will pick:<br/>
+                        <div style={{display: "grid", gridTemplateColumns: "10px auto", columnGap: "10px", marginTop: "10px", marginBottom: "10px"}}>
+                            <div style={{width: "8px", height: "100%", borderRadius: "30px", backgroundColor: "grey"}} />
+                            <p>
+                                <div style={{display: "flex", alignItems: "center", marginBottom: "5px"}}>
+                                <img src={dnd} alt="dnd" style={{"height": "14pt", verticalAlign: "middle", marginRight: 8}} /><span>Do Not Disturb</span>
+                                </div>
+                                <div style={{display: "flex", alignItems: "center", marginBottom: "5px"}}>
+                                <img src={idle} alt="dnd" style={{"height": "14pt", verticalAlign: "middle", marginRight: 8}} /><span>Idle</span>
+                                </div>
+                                <div style={{display: "flex", alignItems: "center", marginBottom: "5px"}}>
+                                <img src={online} alt="dnd" style={{"height": "14pt", verticalAlign: "middle", marginRight: 8}} /><span>Online</span>
+                                </div>
+                            </p>
+                        </div>
+                    </p>
+                } /><br/>
                 
             </div>
         </Page>
